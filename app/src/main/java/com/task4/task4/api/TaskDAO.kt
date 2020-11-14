8beff04892d476417e531c7acb94fb0989988593
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-11-14 15:59:40 -0800

    Made initial DAO

--- app/src/main/java/com/task4/task4/api/TaskDAO.kt
+++ app/src/main/java/com/task4/task4/api/TaskDAO.kt
@@ -1,3 +1,22 @@
 package com.task4.task4.api
 
-interface TaskDAO {}
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import java.util.UUID
+
+@Dao
+interface TaskDAO {
+
+    @Transaction
+    @Query("SELECT * FROM Task")
+    fun getTasksWithSubtasks(): LiveData<List<TaskWithSubTasks>>
+
+    @Query("SELECT * FROM Task WHERE id=(:id)")
+    fun getTask(id: UUID): LiveData<Task?>
+
+    @Update
+    fun updateTask(task: Task)
+
+    @Insert
+    fun createTask(task: Task)
+}
