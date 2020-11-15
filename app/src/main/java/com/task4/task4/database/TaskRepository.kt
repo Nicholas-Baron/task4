db4f69f21011fd6843bac7466b2604592fe64e4c
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-11-14 16:32:28 -0800

    Made initial task repository

---
+++ app/src/main/java/com/task4/task4/database/TaskRepository.kt
@@ -0,0 +1,46 @@
+package com.task4.task4.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+import java.util.UUID
+import java.util.concurrent.Executors
+
+private const val DATABASE_NAME = "task-database"
+
+class TaskRepository private constructor(context: Context) {
+
+    private val database: TaskDatabase = Room.databaseBuilder(
+        context.applicationContext, TaskDatabase::class.java, DATABASE_NAME
+    ).build()
+
+    private val taskDAO: TaskDAO = database.taskDAO()
+
+    private val executor = Executors.newSingleThreadExecutor()
+
+    // CRUD time
+
+    fun createTask(task: Task) {
+        executor.execute { taskDAO.createTask(task) }
+    }
+
+    fun getTasksWithSubtasks(): LiveData<List<TaskWithSubTasks>> = taskDAO.getTasksWithSubtasks()
+
+    fun getTask(uuid: UUID): LiveData<Task?> = taskDAO.getTask(uuid)
+
+    fun update(task: Task) {
+        executor.execute { taskDAO.updateTask(task) }
+    }
+
+    companion object {
+
+        private var INSTANCE: TaskRepository? = null
+
+        fun initialize(context: Context) {
+            if (INSTANCE == null) INSTANCE = TaskRepository(context)
+        }
+
+        fun get(): TaskRepository =
+            INSTANCE ?: throw IllegalStateException("Task repository not initialized")
+    }
+}
