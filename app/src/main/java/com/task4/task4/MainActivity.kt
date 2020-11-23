a2513b3d2121046f5143c8d7b5b0e249ca7ed91d
Arturo Blandon <arturob1024@gmail.com>
2020-11-23 15:10:26 -0800

    Added placeholder code for linking detailView to the listview

--- app/src/main/java/com/task4/task4/MainActivity.kt
+++ app/src/main/java/com/task4/task4/MainActivity.kt
@@ -2,11 +2,18 @@ package com.task4.task4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //TODO: override this function with task list callbacks
+    fun onTaskSelected(taskId: UUID) {
+        val fragment = TaskDetailFragment.newInstance(taskId)
+        TODO("Blocked by pull request on taskView")
     }
 }
