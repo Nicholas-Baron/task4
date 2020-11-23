cc4e1af2adc3108a8293c16b8c2708a6943414af
JamesGaier <james.gaier003@gmail.com>
2020-11-22 21:22:09 -0800

    ui fragment

--- app/src/main/java/com/task4/task4/MainActivity.kt
+++ app/src/main/java/com/task4/task4/MainActivity.kt
@@ -2,11 +2,29 @@ package com.task4.task4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.UUID
 
-class MainActivity : AppCompatActivity() {
-
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity(), TaskListFragment.Callbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get current fragment
+        val currentFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if(currentFragment == null) {
+            val fragment =
+                TaskListFragment.newInstance()
+
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
+    }
+    override fun onTaskSelected(crimeId: UUID) {
+
     }
 }
---
+++ app/src/main/java/com/task4/task4/TaskListFragment.kt
@@ -0,0 +1,130 @@
+package com.task4.task4
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.lifecycle.Observer
+import com.task4.task4.database.Task
+import java.text.DateFormat
+import java.util.*
+
+private const val TAG = "TaskListFragment"
+
+class TaskListFragment : Fragment() {
+    // the list of tasks presented to the user
+    private lateinit var taskRecyclerView: RecyclerView
+    // adapts the list of tasks  from the database for the recycler view
+    private var adapter: TaskAdapter? = TaskAdapter(emptyList())
+
+    // preserve data across rotations lazy load it in
+    private val taskListViewModel: TaskListViewModel by lazy {
+        ViewModelProviders.of(this).get(TaskListViewModel::class.java)
+    }
+
+    private inner class TaskHolder(view: View)
+        : RecyclerView.ViewHolder(view), View.OnClickListener {
+        private lateinit var task: Task
+        private val titleTextView: TextView =
+            itemView.findViewById(R.id.task_title)
+        private val dateTextView: TextView = itemView.findViewById(R.id.task_due_date)
+
+        fun bind(task: Task) {
+            this.task = task
+            titleTextView.text = this.task.name
+            dateTextView.text = DateFormat.getDateInstance().format(this.task.dueDate)
+        }
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            callbacks?.onTaskSelected(task.id)
+        }
+    }
+
+    interface Callbacks {
+        fun onTaskSelected(taskId: UUID)
+    }
+
+    private var callbacks: Callbacks? = null
+
+
+    private inner class TaskAdapter(var tasks: List<Task>)
+        : RecyclerView.Adapter<TaskHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_task, parent, false)
+            return TaskHolder(view)
+        }
+
+        override fun getItemCount() = tasks.size
+
+        override fun onBindViewHolder(holder: TaskHolder, position: Int) {
+            val task = tasks[position]
+
+            holder.bind(task)
+        }
+
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callbacks = context as Callbacks?
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view =
+            inflater.inflate(R.layout.fragment_task_list, container, false)
+        taskRecyclerView =
+            view.findViewById(R.id.task_recycler_view) as RecyclerView
+
+        taskRecyclerView.layoutManager = LinearLayoutManager(context)
+        taskRecyclerView.adapter = adapter
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        taskListViewModel.taskListLiveData.observe(
+            viewLifecycleOwner,
+            { tasks ->
+                tasks?.let {
+                    Log.i(TAG, "Got tasks ${tasks.size}")
+                    updateUI(tasks)
+                }
+
+            }
+        )
+    }
+
+    // remove the reference to the callback receiver
+    override fun onDetach() {
+        super.onDetach()
+        callbacks = null
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_task_list, menu)
+    }
+    private fun updateUI(tasks: List<Task>) {
+        adapter = TaskAdapter(tasks)
+        taskRecyclerView.adapter = adapter
+    }
+
+    companion object {
+        fun newInstance(): TaskListFragment {
+            return TaskListFragment()
+        }
+    }
+}
